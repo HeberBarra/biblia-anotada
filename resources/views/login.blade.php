53a6bf8ec085727772e90d2f0a155d6e7d9f2f78
Heber F Barra <heberferreirabarra@gmail.com>
2025-08-31 17:46:25 -0300

    [ FEATURE ] Estilização da página de Login/Cadastro

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,53 +1,62 @@
 @extends("layouts.main_layout")
 
+@section("head")
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+  <script src="{{ asset('javascript/login.js') }}" defer></script>
+@endsection
+
 @section("content")
+  <div id="wrapper-forms">
     <div id="form-login">
-        <form action="/loginSubmit" method="post">
-            @csrf
-            <h2>Login</h2>
-            <label>Nome de usuário: <input type="text" name="username" required value="{{ old('username') }}"></label>
-            @error('username')
-            <div class="error-form">{{ $message  }}</div>
-            @enderror
-            <label>Senha: <input type="password" name="password" required value="{{ old('password') }}"></label>
-            @error('password')
-            <div class="error-form">{{ $message  }}</div>
-            @enderror
-            <button type="submit">ENVIAR</button>
-        </form>
-        <div id="aviso-login">
-            <h2>Que bom vê-lo(a)</h2>
-            Já possui uma conta? Seja bem-vindo de volta!
-            <button type="button" id="mudar-aviso-login">Faça login.</button>
-        </div>
+      <form action="/loginSubmit" method="post">
+        @csrf
+        <h2>Login</h2>
+        <label>Nome de usuário: <input type="text" name="username" required value="{{ old('username') }}"></label>
+        @error('username')
+        <div class="error-form">{{ $message  }}</div>
+        @enderror
+        <label>Senha: <input type="password" name="password" required value="{{ old('password') }}"></label>
+        @error('password')
+        <div class="error-form">{{ $message  }}</div>
+        @enderror
+        <button type="submit">ENVIAR</button>
+      </form>
     </div>
     <div id="form-cadastro">
-        <form action="/registerSubmit" method="post">
-            @csrf
-            <h2>Registrar-se</h2>
-            <label>Nome de usuário: <input type="text" name="username" required value="{{ old('username') }}"></label>
-            @error('username')
-            <div class="error-form">{{ $message  }}</div>
-            @enderror
-            <label>E-Mail: <input type="email" name="email" required value="{{ old('email') }}"></label>
-            @error('email')
-            <div class="error-form">{{ $message  }}</div>
-            @enderror
-            <label>Senha: <input type="password" name="password" required value="{{ old('password') }}"></label>
-            @error('password')
-            <div class="error-form">{{ $message  }}</div>
-            @enderror
-            <label>Confirmar senha: <input type="password" name="confirm-password" required
-                                           value="{{ old("confirm-password") }}"></label>
-            @error('confirm-password')
-            <div class="error-form">{{ $message  }}</div>
-            @enderror
-            <button type="submit">ENVIAR</button>
-        </form>
-        <div id="aviso-cadastro">
-            <h2>Olá</h2>
-            Ainda não possui uma conta?
-            <button type="button" id="mudar-aviso-cadastro">Cadastre-se</button>
-        </div>
+      <form action="/registerSubmit" method="post">
+        @csrf
+        <h2>Registrar-se</h2>
+        <label>Nome de usuário: <input type="text" name="username" required value="{{ old('username') }}"></label>
+        @error('username')
+        <div class="error-form">{{ $message  }}</div>
+        @enderror
+        <label>E-Mail: <input type="email" name="email" required value="{{ old('email') }}"></label>
+        @error('email')
+        <div class="error-form">{{ $message  }}</div>
+        @enderror
+        <label>Senha: <input type="password" name="password" required value="{{ old('password') }}"></label>
+        @error('password')
+        <div class="error-form">{{ $message  }}</div>
+        @enderror
+        <label>Confirmar senha: <input type="password" name="confirm-password" required
+                                       value="{{ old("confirm-password") }}"></label>
+        @error('confirm-password')
+        <div class="error-form">{{ $message  }}</div>
+        @enderror
+        <button type="submit">ENVIAR</button>
+      </form>
     </div>
+    <div id="overlay">
+      <div id="aviso-cadastro">
+        <h2>Olá!</h2>
+        <p>Ainda não possui uma conta?</p>
+        <button type="button" id="mudar-aviso-cadastro">CADASTRE-SE</button>
+      </div>
+      <div id="aviso-login">
+        <h2>Que bom vê-lo(a)!</h2>
+        <p>Já possui uma conta? Seja bem-vindo de volta!</p>
+        <button type="button" id="mudar-aviso-login">LOGIN</button>
+      </div>
+    </div>
+  </div>
 @endsection
