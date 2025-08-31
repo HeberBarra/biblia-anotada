e0ab0f3a6f8d3c70f76a648150bbb0b12fe88ddc
Heber F Barra <heberferreirabarra@gmail.com>
2025-08-30 23:44:26 -0300

    [ FEATURE ] Versão inicial da página de login/cadastro

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get("login", [AuthController::class, "login"])->name("login");
+Route::post("loginSubmit", [AuthController::class, "loginSubmit"])->name("login.submit");
+Route::post("registerSubmit", [AuthController::class, "registerSubmit"])->name("register.submit");
