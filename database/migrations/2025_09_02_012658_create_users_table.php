6e1d9d4f00deec3c0f288f92df06efa7a64da1c1
Heber F Barra <heberferreirabarra@gmail.com>
2025-09-02 20:49:12 -0300

    [ FEATURE ] Configuração inicial do Banco de Dados

--- database/migrations/2025_09_02_012658_create_users_table.php
+++ database/migrations/2025_09_02_012658_create_users_table.php
@@ -12,9 +12,9 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string("username")->unique();
-            $table->string("email")->unique();
-            $table->string("password");
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
