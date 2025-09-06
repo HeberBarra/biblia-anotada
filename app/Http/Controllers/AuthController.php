1f3afcbda0c2807925f4d1da1d1572ba736875a5
Heber F Barra <heberferreirabarra@gmail.com>
2025-09-06 00:33:30 -0300

    [ FEATURE ] Agora o sistema permite efetuar o login, mas ainda não é possível realizar um cadastro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,27 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Foundation\Application;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
 
-    public function login(): View|Application|Factory
+    public function login(): View
     {
         return view("login");
     }
 
-    public function loginSubmit(Request $request): void
+    public function loginSubmit(Request $request): RedirectResponse
     {
         $request->validate(
             [
                 "username" => "required",
                 "password" => "required|min:8|max:32"
             ]
+        // TODO: Adicionar mensagens de erro traduzidas
         );
+
+        $username = trim($request->input('username'));
+        $password = trim($request->input('password'));
+
+
+        if (Auth::attempt(
+            [
+                'username' => $username,
+                'password' => $password,
+                fn(Builder $query) => $query->whereNull('deleted_at')
+            ])
+        ) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('index');
+        }
+
+        return back()->withErrors([
+            'username' => 'Nome de usuário e/ou senha inválidos.'
+        ])->onlyInput('username');
     }
 
     public function registerSubmit(Request $request): void
@@ -35,6 +57,17 @@ class AuthController extends Controller
                 "confirm-password" => "required|min:8|max:32"
             ]
         );
+
+        // TODO: Fazer sistema de cadastro
+    }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('login');
     }
 
 }
--- database/migrations/2025_09_02_012658_create_users_table.php
+++ database/migrations/2025_09_02_012658_create_users_table.php
@@ -11,12 +11,12 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement();
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
