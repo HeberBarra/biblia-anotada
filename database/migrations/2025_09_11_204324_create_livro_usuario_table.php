0d2f3659b41d0907b45a7653c65fd05cd79a4d68
Douglas Eduardo Ribeiro <20233024971@estudantes.ifpr.edu.br>
2025-09-11 18:02:06 -0300

    criacoes das migrations

---
+++ database/migrations/2025_09_11_204324_create_livro_usuario_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livro_usuario', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('codigo_livro');
+            $table->foreign('codigo_livro')->references('id')->on('livro');
+            $table->unsignedBigInteger('codigo_usuario');
+            $table->foreign('codigo_usuario')->references('id')->on('users');
+            $table->integer('qntd_vezes_lidas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livro_usuario');
+    }
+};
