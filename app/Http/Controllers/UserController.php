9e632f12d040fa3b6ddfa9e0ca18196eed99dab9
Heber F Barra <heberferreirabarra@gmail.com>
2025-10-05 16:13:18 -0300

    [ FEATURE ] Criação de página de listagem de usuários

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::withoutTrashed()->get();
+        return view('users', compact('users'));
     }
 
     /**
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
@@ -6,10 +6,12 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/scss/app.scss',
+                'resources/scss/index.scss',
                 'resources/scss/login.scss',
                 'resources/scss/profile.scss',
+                'resources/scss/users.scss',
                 'resources/typescript/login.ts',
-                'resources/typescript/profile.ts'
+                'resources/typescript/profile.ts',
             ],
             refresh: true,
         }),
