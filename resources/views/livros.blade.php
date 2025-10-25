7d0725513a0d6dbe95cc58ce51f648608763d124
Heber F Barra <heberferreirabarra@gmail.com>
2025-10-24 21:18:49 -0300

    [ ENHANCMENT ] Melhoria na estilização das tabelas de gerenciamento

--- resources/views/livros.blade.php
+++ resources/views/livros.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
   <main>
-    <h2>LIVROS</h2>
+    <h1>LIVROS</h1>
     @if(session()->has('success'))
       <script>
         window.alert('{{ session()->get('success') }}');
@@ -15,52 +15,61 @@
     <table>
       <thead>
       <tr>
-        <th scope="col">ID</th>
-        <th scope="col">NOME</th>
-        <th scope="col">QUANTIDADE DE CAPÍTULOS</th>
-        <th scope="col">CATEGORIA</th>
-        <th scope="col">EDITAR</th>
-        <th scope="col">DELETAR</th>
+        <th scope="col" style="width: 5rem">ID</th>
+        <th scope="col" style="width: 10rem">NOME</th>
+        <th scope="col" style="width: 10rem">CAPÍTULOS</th>
+        <th scope="col" style="width: 10rem">CATEGORIA</th>
+        <th scope="col" style="width: 5rem">EDITAR</th>
+        <th scope="col" style="width: 5rem">DELETAR</th>
       </tr>
       </thead>
-      <body>
-      @foreach($livros as $livro)
-        <tr>
-          <th scope="row">{{ $livro->id }}</th>
-          <td>{{ $livro->nome }}</td>
-          <td>{{ $livro->qntd_capitulos }}</td>
-          <td>{{ $livro->categoriaLivro->nome}}</td>
-          <td class="td-btn">
-            <form action="{{ route('livros.edit', $livro) }}" method="get">
-              <button style="background: none; border: none">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                     class="feather feather-edit">
-                  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                </svg>
-              </button>
-            </form>
-          </td>
-          <td class="td-btn">
-            <form action="{{ route('livros.destroy', $livro) }}" method="post">
-              @csrf
-              @method('DELETE')
-              <button style="background: none; border: none">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                     class="feather feather-trash-2">
-                  <polyline points="3 6 5 6 21 6"></polyline>
-                  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                  <line x1="10" y1="11" x2="10" y2="17"></line>
-                  <line x1="14" y1="11" x2="14" y2="17"></line>
-                </svg>
-              </button>
-            </form>
-          </td>
-        </tr>
-      @endforeach
-      </body>
+      <tbody>
+      <tr>
+        <td colspan="6">
+          <div>
+            <table>
+              @foreach($livros as $livro)
+                <tr id="wrapper">
+                  <th style="width: 5rem" scope="row">{{ $livro->id }}</th>
+                  <td style="width: 10rem">{{ $livro->nome }}</td>
+                  <td style="text-align: center; width: 10rem">{{ $livro->qntd_capitulos }}</td>
+                  <td style="width: 10rem">{{ $livro->categoriaLivro->nome}}</td>
+                  <td style="width: 5rem" class="td-btn">
+                    <form action="{{ route('livros.edit', $livro) }}" method="get">
+                      <button style="background: none; border: none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             class="feather feather-edit">
+                          <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                          <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                        </svg>
+                      </button>
+                    </form>
+                  </td>
+                  <td style="width: 5rem" class="td-btn">
+                    <form action="{{ route('livros.destroy', $livro) }}" method="post">
+                      @csrf
+                      @method('DELETE')
+                      <button style="background: none; border: none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             class="feather feather-trash-2">
+                          <polyline points="3 6 5 6 21 6"></polyline>
+                          <path
+                            d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                          <line x1="10" y1="11" x2="10" y2="17"></line>
+                          <line x1="14" y1="11" x2="14" y2="17"></line>
+                        </svg>
+                      </button>
+                    </form>
+                  </td>
+                </tr>
+              @endforeach
+            </table>
+          </div>
+        </td>
+      </tr>
+      </tbody>
     </table>
     <div id="wrapper-buttons">
       <button type="button">
